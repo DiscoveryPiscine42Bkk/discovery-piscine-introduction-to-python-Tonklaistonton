772409f85101e0a1a988fbd380457eaf00e1cc9a
Tonton <phanuwatgamer158@gmail.com>
2025-06-17 04:14:06 +0000

    add age.py

---
+++ cell04/ex01/age.py
@@ -0,0 +1,6 @@
+age_str = input("Please tell me your age: ").strip()
+age =int(age_str)
+print(f"You are {age} years old.")
+print(f"In 10 years you will be {age+10} years old.")
+print(f"In 20 years you will be {age+20} years old.")
+print(f"In 20 years you will be {age+30} years old.")
