e7eed7fd200e90a4745b81805107f2c6d5fa58dc
Tonton <phanuwatgamer158@gmail.com>
2025-06-17 04:35:21 +0000

    add float.py

---
+++ cell04/ex02/float.py
@@ -0,0 +1,6 @@
+number = input("Give me a number: ")
+number = float(number)
+if number == int(number):
+        print("This number is an integer.")
+else:
+        print("This number is a decimal.")
