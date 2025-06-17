2ad4f71320f19e1df07753e29cb4e9f23adf281a
Tonton <phanuwatgamer158@gmail.com>
2025-06-17 04:45:34 +0000

    add calculator.py

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,13 @@
+first_num =  input ("Enter the first number:").strip()
+second_num =  input ("Enter the second number:").strip()
+first_num = int(first_num)
+second_num = int(second_num)
+print("Thank you!")
+whole_num_1 = first_num + second_num
+whole_num_2 = first_num - second_num
+whole_num_3 = first_num / second_num
+whole_num_4 = first_num * second_num
+print("", first_num, "+", second_num, "=", whole_num_1)
+print("", first_num, "-", second_num, "=", whole_num_2)
+print("", first_num, "/", second_num, "=", whole_num_3)
+print("", first_num, "*", second_num, "=", whole_num_4)
