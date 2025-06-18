323fea4c95411feb22e4501aed3fc04937ee6b6f
Tonton <phanuwatgamer158@gmail.com>
2025-06-18 12:44:00 +0000

    add count_it.py

---
+++ cell05/ex11/count_it.py
@@ -0,0 +1,9 @@
+import shlex
+user_input = input()
+args = shlex.split(user_input)
+if len(args) == 0:
+    print("$")
+else:
+    print(f"parameters: {len(args)}$")
+for param in args:
+    print(f"{param} : {len(param)}$")
